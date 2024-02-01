f1b4f29f333eec8c5c1a7c362873ca89f44e592c
Pai <paaai@icloud.com>
2024-02-01 12:21:36 -0600

    I think the code is correct but the dev env is not. Will test on Linux

--- extractor.js
+++ extractor.js
@@ -1,13 +1,17 @@
 const decompress = require('decompress');
 
 const decompressZIP = (sourcePath, destinationPath) => {
-    decompress(sourcePath, destinationPath)
-        .then(files => {
-            console.log('Successfully extracted the data');
-        })
-        .catch(error => {
-            console.error('Error during decompression:', error);
-        });
+    return new Promise((resolve, reject) => {
+        decompress(sourcePath, destinationPath)
+            .then(files => {
+                console.log('Successfully extracted the data');
+                resolve(files);
+            })
+            .catch(error => {
+                console.error('Error during decompression:', error);
+                reject(error);
+            });
+    });
 };
 
 module.exports = { decompressZIP };
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { downloadPDF } = require('./webscrapper');
 const { pdfToExcel } = require('./formatter');
+const { decompressZIP } = require('./extractor');
 
 const URL = 'https://www.pima.go.cr/boletin/';
 
@@ -12,6 +13,10 @@ async function main() {
         console.log('Starting PDF to Excel conversion...');
         await pdfToExcel();
         console.log('PDF to Excel conversion completed.');
+
+        console.log('Starting ZIP decompression...');
+        await decompressZIP('data/spreadsheet/2024-02-01.zip', 'database');
+        console.log('ZIP decompression completed.');
 
         // Add additional steps here
 
